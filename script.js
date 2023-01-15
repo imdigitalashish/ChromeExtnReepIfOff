698a7d5f17d91ab35ac20c39ed184d6185b2e2c9
imdigitalashish <imdigitalashish@gmail.com>
2023-01-15 22:23:05 +0530

    Remover Logic Done

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function start() {
-    console.log("hello world");
-}
+document.addEventListener("keydown", (e) => {
+    console.log(e)
+})
 
-start();
+
+
+window.onload = () => {
+
+    if (location.href.includes("byjus.com")) {
+        let inteval = setInterval(() => {
+
+            try {
+                document.querySelector(".shaka-play-button").remove();
+
+                clearInterval(inteval);
+            } catch (e) {
+                console.log(e)
+            }
+
+
+        }, 2)
+    }
+}
