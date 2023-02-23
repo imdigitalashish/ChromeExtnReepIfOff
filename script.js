73fe5f328f2dc59ccbf4c2caf355f7ea7ea5929f
imdigitalashish <imdigitalashish@gmail.com>
2023-02-23 23:30:19 +0530

    Youtube

--- script.js
+++ script.js
@@ -17,4 +17,54 @@ window.onload = () => {
 
         }, 2)
     }
+
+    if (location.href.includes("youtube.com")) {
+        let inteval = setInterval(() => {
+
+            try {
+                document.querySelector(".ytp-chrome-top").remove();
+
+                clearInterval(inteval);
+            } catch (e) {
+                console.log(e)
+            }
+
+
+        }, 2)
+    }
+
+    if (location.href.includes("twitter.com")) {
+        let inteval = setInterval(() => {
+
+            try {
+                document.querySelector(".css-4rbku5").remove();
+
+                clearInterval(inteval);
+            } catch (e) {
+                console.log(e)
+            }
+
+
+        }, 2)
+    }
+
+
+
+
+}
+
+
+if (location.href.includes("instagram.com")) {
+    let inteval = setInterval(() => {
+
+        try {
+            document.querySelector("body").remove();
+
+            clearInterval(inteval);
+        } catch (e) {
+            console.log(e)
+        }
+
+
+    }, 2)
 }
